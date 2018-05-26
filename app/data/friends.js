440c74a0070aa903657c332df8e77b3119c394ee
Naeem Ali <alinaeem4321@gmail.com>
2018-05-25 23:35:22 -0500

    files added

---
+++ app/data/friends.js
@@ -0,0 +1,163 @@
+var friendsArray = [{
+        name: 'Naeem Ali',
+        photo: 'https://user-images.githubusercontent.com/34846225/40456191-a9a8cf9e-5eb5-11e8-9c08-159991653dc4.jpg',
+        scores: [
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5'
+        ]
+    },
+    {
+        name: 'Sandra',
+        photo: 'https://ca.slack-edge.com/T8WSMDU3A-U9BH6AW4S-8ba16fcef568-1024',
+        scores: [
+            '1',
+            '5',
+            '5',
+            '1',
+            '5',
+            '5',
+            '1',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Jose Flores',
+        photo: 'https://ca.slack-edge.com/T8WSMDU3A-U9DDD0VNY-822701295e60-1024',
+        scores: [
+            '2',
+            '4',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '2',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Ben',
+        photo: 'https://ca.slack-edge.com/T8WSMDU3A-U9A1AUTRA-e2bd5ed49bea-1024',
+        scores: [
+            '4',
+            '3',
+            '5',
+            '1',
+            '1',
+            '5',
+            '5',
+            '1',
+            '1',
+            '1'
+        ]
+    },
+    {
+        name: 'Elaulas Cristiano',
+        photo: 'https://ca.slack-edge.com/T8WSMDU3A-U8YJSNSCX-338c874e754e-1024',
+        scores: [
+            '5',
+            '1',
+            '2',
+            '1',
+            '3',
+            '4',
+            '1',
+            '3',
+            '3',
+            '4'
+        ]
+    },
+    {
+        name: 'Barack Obama',
+        photo: 'http://1.bp.blogspot.com/-515irK7ZyVA/UOkEWfSfaRI/AAAAAAAAA3k/ilB7zoK4gFc/s1600/Obama+official+hd+wallpaper.jpg',
+        scores: [
+            '1',
+            '1',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '5',
+            '1'
+        ]
+    },
+    {
+        name: 'Donald Trump',
+        photo: 'http://cdn-image.travelandleisure.com/sites/default/files/styles/1600x1000/public/trump0715.jpg?itok=ZOEWi1UB',
+        scores: [
+            '1',
+            '1',
+            '5',
+            '1',
+            '3',
+            '2',
+            '5',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Tim Stevens',
+        photo: 'https://ca.slack-edge.com/T8WSMDU3A-U8X0V2NM6-5ee012b7c33f-1024',
+        scores: [
+            '5',
+            '4',
+            '4',
+            '2',
+            '4',
+            '4',
+            '3',
+            '2',
+            '3',
+            '3'
+        ]
+    },
+    {
+        name: 'Damian Ramirez',
+        photo: 'https://ca.slack-edge.com/T8WSMDU3A-U8XQYF6LT-1c294c30095c-1024',
+        scores: [
+            '5',
+            '2',
+            '3',
+            '1',
+            '5',
+            '1',
+            '3',
+            '1',
+            '1',
+            '5'
+        ]
+    },
+    {
+        name: 'Haoran(Steven) Xu',
+        photo: 'https://ca.slack-edge.com/T8WSMDU3A-U8XP573QD-16ae7e11ea10-1024',
+        scores: [
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1',
+            '1'
+        ]
+    }
+];
+
+module.exports = friendsArray;
